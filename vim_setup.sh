aeb8f7bf208243f138b17664d62a8378f0b90c99
Dhole <bankukur@gmail.com>
2014-11-23 13:26:46 +0100

    vim changes

---
+++ vim_setup.sh
@@ -0,0 +1,8 @@
+## Create bundle folder
+mkdir -p .vim/bundle
+
+## Clone Vundle
+git clone git://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
+
+## Install bundles
+vim +BundleInstall +qall
